20af7404d2c300f4b2516bdd0a14fc39e2cb550b
jyldpfla <82352167+jyldpfla@users.noreply.github.com>
2022-05-19 17:10:08 +0900

    Add files via upload

---
+++ 0519/LogicalOper.java
@@ -0,0 +1,16 @@
+// and 연산자
+public class LogicalOper {
+	public static void main(String[] args) {
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+		
+// or 연산자		
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+	}
+}
+
---
+++ 0519/RelOper.java
@@ -0,0 +1,12 @@
+// 관계연산자
+
+public class RelOper {
+	public static void main(String[] args) {
+		System.out.println(55 == 55); // 두 값이 같은지를 물어보는 연산자 -> 결과값 true,false로 나옴
+		System.out.println(55 != 55); // 두 값이 다른지를 물어봄
+		System.out.println(55 > 55); // 왼쪽 값이 큰지
+		System.out.println(55 < 55); // 오른쪽 값이 큰지
+		System.out.println(55 >= 55); // 왼쪽 값이 크거나 같은지
+		System.out.println(55 <= 55); // 오른쪽 값이 크거나 같은지
+	}
+}
