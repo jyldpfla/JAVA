db5f3a164890399e761daf9db645c89ef8057fd9
jyldpfla <82352167+jyldpfla@users.noreply.github.com>
2022-08-16 17:27:18 +0900

    Delete 0519 directory

--- 0519/BooleanType.java
+++
@@ -1,9 +0,0 @@
-public class BooleanType {
-	public static void main(String[] args) {
-		boolean t = true; // 참이라는 값을 초기화, java에서는 true = 1, false = 0 으로 표현 x -> 숫자로 표현 불가
-		boolean f= false;
-		
-		System.out.println(t);
-		System.out.println(f);
-	}
-}
